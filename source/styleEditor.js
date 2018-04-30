506bcffaeb81be47540883d4e48fcd51aa73baa0
Luke Chambers <lukenatchambers@gmail.com>
2018-04-29 19:10:30 -0600

    Finished commit

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  // your code goes here.
+  $(".submit").click(function (event)) {
+    var property = $("input#property");
+    var value = $("input#value");
+    var selector = $("input#selector");
+    $(selector.val()).css(property.val(), value.val();
+    event.preventDefault
+  });
 });
